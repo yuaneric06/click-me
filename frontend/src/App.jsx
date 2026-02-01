b4f3c586e24349bbacd45ceaa0b92ed492373eed
Eric Yuan <yuaneric6@gmail.com>
2026-02-01 08:03:22 -0500

    autoclicker checker + enable popup msg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 export default function App() {
   const [clicksLeft, setClicksLeft] = useState(1000);
+  const [popupsAllowed, setPopupsAllowed] = useState(false);
+  const [usingAutoclicker, setUsingAutoclicker] = useState(true);
+  const prevClickTime = useRef(null);
+
+  useEffect(() => {
+    const checkPopups = () => {
+      const testPopup = window.open("", "_blank");
+      if (!testPopup) {
+        setPopupsAllowed(false);
+      }
+      else {
+        testPopup.close();
+        setPopupsAllowed(true);
+      }
+    }
+    checkPopups();
+  }, []);
 
   const handleClick = () => {
-    setClicksLeft((currClicks) => {
-      return currClicks - 1;
-    });
-    setTimeout(() => {
-      window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank");
-    }, 10000);
+    const currTime = Date.now();
+    console.log("curr time: ", currTime);
+    console.log("prev time: ", prevClickTime.current);
+    if (prevClickTime.current !== null && currTime - prevClickTime.current < 100) {
+      setUsingAutoclicker(true);
+      setClicksLeft((currClicks) => {
+        return currClicks - 1;
+      });
+      setTimeout(() => {
+        window.open("https://yuaneric06.github.io/click-me", "_blank");
+      }, 10000);
+    }
+    else {
+      setClicksLeft(1000);
+      setUsingAutoclicker(false);
+    }
+    prevClickTime.current = currTime;
   }
+
   return (
     <main>
-      <button onClick={handleClick}>Click me {String(clicksLeft)} times</button>
+      {popupsAllowed ? <button onClick={handleClick}>Click me {String(clicksLeft)} times</button> : <p>Enable popups for this page and reload</p>}
+      {!usingAutoclicker && <p>Use <a color="white" href="https://sourceforge.net/projects/orphamielautoclicker/" target="_blank">this</a> autoclicker, set the delay to 1 ms</p>}
     </main>
   )
 }
